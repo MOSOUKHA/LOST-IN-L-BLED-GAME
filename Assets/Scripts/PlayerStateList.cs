826d6a6c948bf856a4a1a61fe9ce82f7cb79dacf
Mostafa OUKHA <mostafa.oukha.23@ump.ac.ma>
2026-04-30 11:40:37 +0100

    add unity project with character

---
+++ Assets/Scripts/PlayerStateList.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateList : MonoBehaviour
+{
+    
+    public bool jumping = false;
+    public bool dashing = false;
+    public bool recoilingX = false;
+    public bool recoilingY = false;
+    public bool lookingRight = false;
+    public bool invincible = false;
+    public bool alive = true;
+    public bool healing;
+    public bool casting;
+
+}
